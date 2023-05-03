8a0940b53ba029465b89b92a7217187a8dff3b03
hongxiang <xxxxxxx@xxx.com>
2023-05-03 01:36:09 -0400

    ContractL Ticket_Market/1

--- contracts/Ticket_Market.sol
+++ contracts/Ticket_Market.sol
@@ -1,1 +1,21 @@
 pragma solidity ^0.8.0;
+
+import "./Ticket_Factory.sol";
+import "./TIcket_Hong.sol";
+
+contract FestivalMarketplace {
+    TIcket_Hong private _token;
+    Ticket_Factory private _NFT;
+
+    address private _organiser;
+
+    constructor(TIcket_Hong token, Ticket_Factory NFT) public {
+        _token = token;
+        _NFT = NFT;
+        _organiser = _NFT.getOrganiser();
+    }
+
+    event Purchase(address indexed buyer, address seller, uint256 ticketId);
+
+
+}
