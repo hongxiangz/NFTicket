f69b04d2cb6a6af7c9cb4c23ef4f6c9f5a0cdb86
hongxiang <xxxxxxx@xxx.com>
2023-05-04 20:28:59 -0400

    Ticket_Factory.sol/ struct construction

--- contracts/Ticket_Factory.sol
+++ contracts/Ticket_Factory.sol
@@ -3,3 +3,14 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Ticket_Nft.sol";
 import "./Ticket_Market.sol";
+
+contract Ticket_Factory is Ownable {
+    struct Ticket {
+        string activities_name;
+        string ticketSymbol;
+        uint256 ticketPrice;
+        uint256 totalSupply;
+        address marketplace;
+    }
+
+}
