6e2e70b6eeaa09a824cabe5e22abc4effa2a5c25
hongxiang <xxxxxxx@xxx.com>
2023-05-04 23:19:18 -0400

    truffle configuration

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,26 @@
 module.exports = {
+  networks: {
+    development: {
+      host: "ganache",   
+      port: 8545,      
+      network_id: "2024",   
+    },
+  },
+
+  db: {
+    enabled: false
+  },
 
   compilers: {
     solc: {
-      version: "^0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "^0.8.0",    
+      docker: false,       
+      settings: {          
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
+        evmVersion: "shanghai"
+      }
     }
   }
-}
